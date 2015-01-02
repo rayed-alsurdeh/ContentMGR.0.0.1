ece307d5c64f75ce42c42700483c319ccb0d4c11
rayed <alsrde_mail@yahoo.com>
2015-01-02 19:14:14 +0300

    commet#1

---
+++ RSContentMGR/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSContentMGR.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        RSContentMGR.Models.CMGRDataContext db = new Models.CMGRDataContext();
+        public ActionResult Index()
+        {
+            var catgs = db.getCtgs();
+            return View(catgs.ToList());
+        }
+
+    }
+}
---
+++ RSContentMGR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RSContentMGR.MvcApplication" Language="C#" %>
